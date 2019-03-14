28edffd73daf63fa4af6cbf8f6692993f3401a17
eatbye <eatbye@gmail.com>
2019-03-14 10:32:03 +0800

    get请求与text组件

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,24 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    getHttp();
-    return Scaffold(
-      body: Center(
-        child: Text('首页'),
-      ),
-    );
-  }
+  _HomePageState createState() => _HomePageState();
+}
 
-  void getHttp() async{
-    try{
-      Response response;
-      response = await Dio().get("https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=test");
-      return print(response);
-    }catch(e){
+class _HomePageState extends State<HomePage> {
+  TextEditingController typeController = TextEditingController();
+
+  String showText = '文字展示';
+
+  Future getHttp(String text) async {
+    try {
+      var data = {'name': text};
+      Response response = await Dio().get(
+          "https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian",
+          queryParameters: data);
+      return response.data;
+    } catch (e) {
       return print(e);
     }
+  }
+
+  void _choiceAction() {
+    print('开始选择');
+    if (typeController.text.toString() == '') {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: Text('内容不能为空'),
+              ));
+    } else {
+      getHttp(typeController.text.toString()).then((value) {
+        setState(() {
+          showText = value['data']['name'].toString();
+        });
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('测试'),
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: typeController,
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(10.0),
+                    labelText: '测试',
+                    helperText: '请输入内容'),
+                autofocus: false,
+              ),
+              RaisedButton(
+                onPressed: _choiceAction,
+                child: Text('确认选择'),
+              ),
+              Text(
+                showText,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
