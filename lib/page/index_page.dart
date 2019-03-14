4dd10c37f34902c66cc542e9c83c01a53f24e4ca
eatbye <eatbye@gmail.com>
2019-03-14 09:19:25 +0800

    底部导航栏

--- lib/page/index_page.dart
+++ lib/page/index_page.dart
@@ -1,14 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_shop/page/cart_page.dart';
+import 'package:flutter_shop/page/category_page.dart';
+import 'package:flutter_shop/page/home_page.dart';
+import 'package:flutter_shop/page/member_page.dart';
 
-class IndexPage extends StatelessWidget {
+class IndexPage extends StatefulWidget {
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  final List<BottomNavigationBarItem> bottomTabs = [
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),
+      title: Text('首页'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.search),
+      title: Text('分类'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.shopping_cart),
+      title: Text('购物车'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.profile_circled),
+      title: Text('会员中心'),
+    ),
+  ];
+
+  final List tabPages = [
+    HomePage(),
+    CategoryPage(),
+    CartPage(),
+    MemberPage(),
+  ];
+
+  int currentIndex = 0;
+  var currentPage;
+
+  @override
+  void initState() {
+    currentPage = tabPages[currentIndex];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('百姓生活+'),
-        ),
-        body: Center(
-          child: Text('百姓生活+'),
-        ));
+      backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        items: bottomTabs,
+        onTap: (index){
+          setState(() {
+            currentIndex = index;
+            currentPage = tabPages[currentIndex];
+          });
+        },
+      ),
+      body: currentPage,
+    );
   }
 }
